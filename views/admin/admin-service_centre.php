42b3d3b14ba980d82441e28ca29cf3753a0937ed
rathinimal <rathinarasanimal@gmail.com>
2025-04-14 14:41:29 +0530

    the change of technician name to service centre

--- views/admin/admin-service_centre.php
+++ views/admin/admin-service_centre.php
@@ -56,7 +56,7 @@ include_once 'components/header.php';
     <!-- Modal -->
     <div id="delete-modal" class="modal hidden">
         <div class="modal-content">
-            <h3>Are you sure you want to delete this technician?</h3>
+            <h3>Are you sure you want to delete this service centre?</h3>
             <div class="modal-buttons">
                 <button id="confirm-delete" class="button failure">Yes</button>
                 <button id="cancel-delete" class="button gray">No, cancel</button>
